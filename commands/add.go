4cdfc36f85124ac836786e75ea6c91b77e679486
N.Ardi <noone.nu1@gmail.com>
2014-12-08 23:50:18 +0700

    Update add command, use utils.

--- commands/add.go
+++ commands/add.go
@@ -1,59 +1,36 @@
 package commands
 
 import (
-	"errors"
 	"github.com/emaniacs/todoin/db"
 	"github.com/emaniacs/todoin/utils"
 	"os"
 )
 
 // TODO: set os.Args as arguments
-func parseAddArgs() (error, *db.Task) {
-	var err error
-	err = nil
-	task := new(db.Task)
-	sliceStart := 0
-
-	if len(os.Args) >= 4 {
-		task.Value = os.Args[2]
-		sliceStart = 3
-		switch os.Args[3] {
-		case "ok", "1":
-			task.Status = 1
-		case "0":
-			task.Status = 0
-		default:
-			task.Status = 0
-		}
-	} else if len(os.Args) >= 3 {
-		sliceStart = 2
-		task.Value = os.Args[2]
-		task.Status = 0
-	} else {
-		err = errors.New("Not enough arguments")
-	}
-
-	if err != nil {
-		for _, arg := range os.Args[sliceStart:] {
-			if utils.IsAssignBy(arg) {
-				task.AssignBy = arg[1:]
-			} else if utils.IsAssignTo(arg) {
-				task.AssignTo = arg[1:]
-			} else if utils.IsDueDate(arg) {
-				task.DueDate = arg[1:]
-			}
+func parseAddArgs(task *db.Task) {
+	for _, arg := range os.Args[2:] {
+		if utils.IsAssignBy(arg) {
+			task.AssignBy = arg[1:]
+		} else if utils.IsAssignTo(arg) {
+			task.AssignTo = arg[1:]
+		} else if utils.IsDueDate(arg) {
+			task.DueDate = arg[1:]
+		} else if status, err := utils.IsDone(arg); err != false {
+			task.Status = status
+		} else if task.Value == "" {
+			task.Value = arg
 		}
 	}
-
-	return err, task
 }
 
 // value status
 func Add() (int, string) {
-	err, task := parseAddArgs()
-	if err != nil {
-		return -1, err.Error()
+	if len(os.Args) < 3 {
+		return -1, "Not enough arguments"
 	}
+
+	task := new(db.Task)
+	parseAddArgs(task)
 
 	key, msg := db.Insert(task)
 
