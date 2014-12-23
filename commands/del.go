15fd4f2ec5221b4fee2ce81a9cc08d8a4f2ebb42
N.Ardi <noone.nu1@gmail.com>
2014-12-23 23:29:12 +0700

    support multiple delete task.

--- commands/del.go
+++ commands/del.go
@@ -19,22 +19,24 @@ func init() {
 			}
 
 			// TODO: remove by column
-			key, err := utils.IsNumeric(args[0])
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Invalid key")
-				return 255
-			}
-			if !db.Exist(key) {
-				fmt.Fprintln(os.Stderr, "Task not found.")
-				return 255
-			}
+			for _, val := range args {
+				key, err := utils.IsNumeric(val)
+				if err != nil {
+					fmt.Fprintf(os.Stdout, "(%s) Invalid key\n", val)
+					continue
+				}
+				if !db.Exist(key) {
+					fmt.Fprintf(os.Stdout, "(%d) Task not found\n", key)
+					continue
+				}
 
-			if db.DeleteById(int64(key)) {
-				fmt.Fprintln(os.Stdout, "Success")
-				return 0
+				if db.DeleteById(int64(key)) {
+					fmt.Fprintf(os.Stdout, "(%d) Success\n", key)
+				} else {
+					fmt.Fprintf(os.Stdout, "(%d) Fail\n", key)
+				}
 			}
-			fmt.Fprintln(os.Stdout, "Fail")
-			return 255
+			return 0
 		},
 	})
 }
