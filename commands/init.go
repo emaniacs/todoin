62000b961ff3124e9b2e9b26433abf83cf77a885
N.Ardi <noone.nu1@gmail.com>
2014-12-27 00:14:10 +0700

    Add usage of init.

--- commands/init.go
+++ commands/init.go
@@ -1,13 +1,19 @@
 package commands
 
 import (
+	"fmt"
 	"github.com/emaniacs/todoin/db"
 )
 
 func init() {
 	Register("init", &Command{
 		Usage: func() string {
-			return "Usage of init"
+			return fmt.Sprintf(`
+Initialize the table.
+This command will re-create the table (task)
+Usage:
+	%s init
+`, appName)
 		},
 		Run: func(args []string) int {
 			db.TableRemove("task")
