48c7eef80ff97c913379b9e803187672d1e9d4e5
N.Ardi <noone.nu1@gmail.com>
2014-12-27 00:33:10 +0700

    Add usage of list.

--- commands/list.go
+++ commands/list.go
@@ -7,7 +7,7 @@ import (
 func init() {
 	Register("list", &Command{
 		Usage: func() string {
-			return "List of command"
+			return "List of commands."
 		},
 		Run: func(args []string) int {
 			for name, _ := range containers {
