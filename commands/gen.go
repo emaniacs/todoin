247aab9907e243d6fe576646c87af866787b71d5
N.Ardi <noone.nu1@gmail.com>
2014-12-26 23:00:51 +0700

    update generate commands.

--- commands/gen.go
+++ commands/gen.go
@@ -10,11 +10,17 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"sync"
 )
 
 var regexString = ".*TODO: +([^@$?!]*)([@$?].*[^ ])?"
+var Ask, Insert *bool
+var fchan = make(chan string)
+var tchan = make(chan *db.Task)
+var Prefix []string
+var Wg sync.WaitGroup
 
-func parseFile(fchan chan string) {
+func parseFile() {
 	re := regexp.MustCompile(regexString)
 	for {
 		fn := <-fchan
@@ -28,58 +34,76 @@ func parseFile(fchan chan string) {
 		line := 1
 		for scanner.Scan() {
 			text := scanner.Text()
-			task := re.FindStringSubmatch(text)
-			if len(task) > 0 {
-				value := task[1]
-				other := ""
-				if len(task) > 2 {
-					other = task[2]
-				}
-				key := addTask(value, other)
-				fmt.Printf("(%d) %s:%s \"%s\"\n", key, fn, line, value)
+			found := re.FindStringSubmatch(text)
+			if len(found) > 0 {
+				Wg.Add(1)
+				tchan <- generateTask(found, fn, line)
 			}
 			line++
 		}
 	}
 }
 
-func parseDir(dirs string, prefixs string, fchan chan string) {
-	for _, dir := range strings.Split(dirs, ",") {
-		dir = strings.TrimRight(dir, "/")
-		dirStat, err := os.Stat(dir)
-		if err != nil {
-			continue
-		}
-
-		if dirStat.IsDir() {
-			for _, prefix := range strings.Split(prefixs, ",") {
-				files, _ := filepath.Glob(dir + "/" + prefix)
-				for _, file := range files {
-					fchan <- file
+func addTask() {
+	for {
+		t := <-tchan
+		if *Insert {
+			fmt.Fprintf(os.Stdout, "[%s:%d \"%s\"] ", t.FileName, t.Line, t.Value)
+			insert := true
+			if *Ask {
+				var yes string
+				fmt.Print("Insert (y/n): ")
+				fmt.Scanf("%s", &yes)
+				if strings.Trim(yes, " ") != "y" {
+					insert = false
 				}
+			} else {
+				fmt.Println("")
+			}
+
+			if insert {
+				key := db.Insert(t)
+				fmt.Printf(" -> %d\n", key)
+			} else {
+				fmt.Println(" -> No")
 			}
 		} else {
-			fchan <- dir
+			fmt.Printf("%s:%s \"%s\"\n", t.FileName, t.Line, t.Value)
 		}
+		Wg.Done()
 	}
 }
 
-func addTask(value string, other string) int64 {
+func generateTask(found []string, fn string, line int) *db.Task {
+	value := found[1]
+
 	task := new(db.Task)
 	task.Value = value
-	for _, val := range strings.Split(other, " ") {
-		if status, ok := utils.IsDone(val); ok {
-			task.Status = status
-		} else if utils.IsAssignBy(val) {
-			task.AssignBy = val[1:]
-		} else if utils.IsAssignTo(val) {
-			task.AssignTo = val[1:]
-		} else if utils.IsDueDate(val) {
-			task.DueDate = val[1:]
+	task.FileName = fn
+	task.Line = line
+	if len(found) > 2 {
+		for _, val := range strings.Split(found[2], " ") {
+			if status, ok := utils.IsDone(val); ok {
+				task.Status = status
+			} else if utils.IsAssignBy(val) {
+				task.AssignBy = val[1:]
+			} else if utils.IsAssignTo(val) {
+				task.AssignTo = val[1:]
+			} else if utils.IsDueDate(val) {
+				task.DueDate = val[1:]
+			}
 		}
 	}
+	return task
+}
 
-	return db.Insert(task)
+func prefixMatch(file string) bool {
+	for _, val := range Prefix {
+		if strings.HasSuffix(file, val) {
+			return true
+		}
+	}
+	return false
 }
 
 func init() {
@@ -92,6 +116,8 @@ func init() {
 			flg.Init("gen", flag.ContinueOnError)
 			paths := flg.String("path", "", "")
 			files := flg.String("file", "", "")
+			Insert = flg.Bool("insert", true, "")
+			Ask = flg.Bool("ask", false, "")
 			flg.Parse(args)
 
 			if len(*paths) < 1 || len(*files) < 1 {
@@ -99,9 +125,27 @@ func init() {
 				return 255
 			}
 
-			fchan := make(chan string)
-			go parseFile(fchan)
-			parseDir(*paths, *files, fchan)
+			for _, pre := range strings.Split(*files, ",") {
+				Prefix = append(Prefix, strings.Replace(pre, "*", "", -1))
+			}
+
+			go addTask()
+			go parseFile()
+			for _, path := range strings.Split(*paths, ",") {
+				Wg.Add(1)
+				go func() {
+					filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+						if !f.IsDir() && prefixMatch(path) {
+							fchan <- path
+						}
+						return nil
+					})
+					Wg.Done()
+				}()
+			}
+			Wg.Wait()
+
+			// parseDir(*paths, *files, f)
 
 			return 255
 		},
