1624cec22493b8487244eaba4d8f87b962008441
N.Ardi <noone.nu1@gmail.com>
2014-12-26 23:54:28 +0700

    update commands.go, move registering help comand into separated file.

--- commands/commands.go
+++ commands/commands.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"flag"
 	"fmt"
+	"github.com/emaniacs/todoin/db"
 	"os"
 )
 
@@ -36,6 +37,12 @@ func Run() {
 		os.Exit(0)
 	}
 
+	// check table
+	if !db.TableExist("task") && os.Args[1] != "init" {
+		fmt.Println("Table not found, please init")
+		os.Exit(255)
+	}
+
 	exit := 255
 	name := os.Args[1]
 	if cmd, ok := Exist(name); ok {
@@ -51,25 +58,6 @@ func Exist(name string) (*Command, bool) {
 
 func Init() {
 	appName = os.Args[0]
-
-	// register for help
-	Register("help", &Command{
-		Usage: func() string {
-			return "This is help"
-		},
-		Run: func(args []string) int {
-			if len(args) > 0 {
-				cmd, ok := Exist(args[0])
-				if ok {
-					fmt.Fprintln(os.Stdout, cmd.Usage())
-					return 0
-				}
-				return 255
-			}
-			fmt.Fprintf(os.Stderr, "this help not helping yet.\n")
-			return 0
-		},
-	})
 }
 
 func parseFlag(name string) *Args {
